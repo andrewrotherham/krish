cbdbc3bb4ae09f2fc93b5d964847eb67a469396b
andrewrotherham <66618878+andrewrotherham@users.noreply.github.com>
2022-06-26 16:58:29 +0100

    Update output.tf

--- Section-05-modules/tf-module-lab-demo/module/webservers-elb-asg/output.tf
+++ Section-05-modules/tf-module-lab-demo/module/webservers-elb-asg/output.tf
@@ -7,11 +7,3 @@ output "my_module_sg_id" {
     description = "outputs webserver security group id"
     value ="${aws_security_group.webserver_sg.id}"
 }
-
-output "asg_id" {
-    value = "${module.webservers.my_asg_id}"
-}
-
-output "asg_name" {
-    value ="${module.webservers.my_asg_name}"
-}
