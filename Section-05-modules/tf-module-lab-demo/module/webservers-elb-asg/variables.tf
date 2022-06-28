ca693f14e24062ea5d0c03aaf327cb7d498d052d
andrewrotherham <andrewmichaelnewman8@gmail.com>
2022-06-28 12:41:21 +0100

    add zone iD hpefuly to 12

--- Section-05-modules/tf-module-lab-demo/module/webservers-elb-asg/variables.tf
+++ Section-05-modules/tf-module-lab-demo/module/webservers-elb-asg/variables.tf
@@ -1,15 +1,19 @@
 variable "environment" {
-description= "type of environment , prod or stg or dev "
-default="stg"
+  description = "type of environment , prod or stg or dev "
+  default     = "stg"
+}
+variable "zone_id" {
+  default = "Z015913437MDX5NSGQWWV"
+
 }
 
 variable "vpc_id" {
-description = "provide vpc_id"
+  description = "provide vpc_id"
 }
 
 variable "instance_type" {
   description = "instance type ..."
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "cluster" {
@@ -20,13 +24,13 @@ variable "region" {
   default = "eu-west-2"
 }
 variable "http_port" {
-    default = 80
+  default = 80
 }
 variable "ssh_port" {
-    default = 22
+  default = 22
 }
 variable "my_system" {
-    default = "0.0.0.0/0"
+  default = "0.0.0.0/0"
 }
 
 variable "ami" {
@@ -34,5 +38,5 @@ variable "ami" {
 }
 
 variable "azs" {
-default = [ "eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  default = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
 }
